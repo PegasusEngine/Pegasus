4febc128795a656d34496b61b105d7be3b885c41
Kleber Garcia <kecho_garcia@hotmail.com>
2014-03-30 10:37:55 -0500

    [UNITTESTS] Adding a nicer header per package structure to unit tests
    [git-p4: depot-paths = "//Pegasus/DL/": change = 843]

--- Source/Pegasus/UnitTests/main.cpp
+++ Source/Pegasus/UnitTests/main.cpp
@@ -4,172 +4,19 @@
 /*                                                                                      */
 /****************************************************************************************/
 
-//! \file   UtilsUnitTests.cpp
+//! \file   main.cpp (unit tests entry point)
 //! \author Kleber Garcia
-//! \date   2/15/2014
+//! \date   3/30/2014
 //! \brief  Set of unit tests, used to prove soundness of 
 //!         any data structure. To run, edit Utils project to generate an executable, and run
 
-#include "Pegasus\Utils\Memcpy.h"
-#include "Pegasus\Utils\String.h"
+#include "Pegasus/UnitTests/UtilsTests.h"
 #include <stdio.h>
 
 typedef bool (*TestFunc)(void);
 
-//********* UNIT TEST DECLARATIONS **********//
 
-bool UNIT_TEST_MemcpyUnitTest1()
-{
-    //Test
-    const char * testString1 = "This is a test string."; int count = 23;//size of memory is 
-    char destBuffer[100];
-    for (int i = 0; i < 100; ++i) destBuffer[i] = 99; //initialize all memory to 99
-    Pegasus::Utils::Memcpy(destBuffer, testString1,count);
-
-    //compare
-    bool match = true;
-    for (int i = 0; i < count; ++i) match = match && testString1[i] == destBuffer[i];
-    for (int i = count; i < 100; ++i) match = match && destBuffer[i] == 99;
-
-    return match;
-}
-
-
-bool UNIT_TEST_MemcpyUnitTest2()
-{
-    //Test 
-    const char * testString1 = ""; int count = 0;
-    char destBuffer[100];
-    for (int i = 0; i < 100; ++i) destBuffer[i] = 21; //initialize all memory to 21
-    Pegasus::Utils::Memcpy(destBuffer, testString1,count);
-
-    //compare
-    bool match = true;    
-    for (int i = 0; i < 100; ++i) match = match && destBuffer[i] == 21;
-
-    return match;
-}
-
-bool UNIT_TEST_MemcpyUnitTest3()
-{
-    //Test 
-    unsigned long long bigList[267];
-    unsigned long long destList[267];
-    for (int i = 0; i < 256; ++i) bigList[i] =  i;
-    Pegasus::Utils::Memcpy(destList, bigList, 256 * sizeof(unsigned long long));
-
-    //compare
-    bool match = true;    
-    for (int i = 0; i < 256; ++i) match = match && destList[i] == i;
-
-    return match;
-}
-
-bool UNIT_TEST_Strcmp1()
-{
-    const char * c1 = "ThisIsAString";
-    const char * c2 = "ThisIsAString";
-    return !Pegasus::Utils::Strcmp(c1, c2);
-}
-
-bool UNIT_TEST_Strcmp2()
-{
-    const char * c1 = "ThisIsAString";
-    const char * c2 = "ThisIsA";
-    return Pegasus::Utils::Strcmp(c1, c2) == -static_cast<int>('S');
-}
-
-bool UNIT_TEST_Strcmp3()
-{
-    const char * c1 = "ThisIsAString";
-    const char * c2 = "ThisIsAstring";
-    return Pegasus::Utils::Strcmp(c1, c2) == static_cast<int>('S' - 's');
-}
-
-bool UNIT_TEST_Strcmp4()
-{
-    const char * c1 = "";
-    const char * c2 = "ThisIsAstring";
-    return Pegasus::Utils::Strcmp(c1, c2) == static_cast<int>('T');
-}
-
-bool UNIT_TEST_Stricmp1()
-{
-    const char * c1 = " \t ThisIsAString+*-&\\";
-    const char * c2 = " \t ThIsISastrIng+*-&\\";
-    return !Pegasus::Utils::Stricmp(c1, c2);
-}
-
-bool UNIT_TEST_Stricmp2()
-{
-    const char * c1 = " \t ThisIsAString+*-& ";
-    const char * c2 = " \t ThIsISastrIng+*-&\\";
-    return Pegasus::Utils::Stricmp(c1, c2) == static_cast<int>(' ' - '\\');
-}
-
-bool UNIT_TEST_Stricmp3()
-{
-    const char * c1 = " ThisIsAString+*-& ";
-    const char * c2 = "";
-    return Pegasus::Utils::Stricmp(c1, c2) == -static_cast<int>(' ');
-}
-
-bool UNIT_TEST_Strlen1()
-{
-    const char * c1 = "1234567890";
-    return Pegasus::Utils::Strlen(c1) == 10;
-}
-
-bool UNIT_TEST_Strlen2()
-{
-    const char * c1 = "";
-    return Pegasus::Utils::Strlen(c1) == 0;
-}
-
-bool UNIT_TEST_Strlen3()
-{
-    const char * c1 = "1";
-    return Pegasus::Utils::Strlen(c1) == 1;
-}
-
-
-bool UNIT_TEST_Strchr1()
-{
-    const char * c1 = "01234567890";
-    return Pegasus::Utils::Strchr(c1, '0') == c1;
-}
-
-bool UNIT_TEST_Strchr2()
-{
-    const char * c1 = "01234567890";
-    return Pegasus::Utils::Strchr(c1, '9') == (c1 + 9);
-}
-
-bool UNIT_TEST_Strchr3()
-{
-    const char * c1 = "01234567890";
-    return Pegasus::Utils::Strchr(c1, '6') == (c1 + 6);
-}
-
-bool UNIT_TEST_Strrchr1()
-{
-    const char * c1 = "01234567890";
-    return Pegasus::Utils::Strrchr(c1, '0') == c1 + 10;
-}
-
-bool UNIT_TEST_Strrchr2()
-{
-    const char * c1 = "01234567890";
-    return Pegasus::Utils::Strrchr(c1, '9') == (c1 + 9);
-}
-
-bool UNIT_TEST_Strrchr3()
-{
-    const char * c1 = "x1234567890";
-    return Pegasus::Utils::Strrchr(c1, 'x') == (c1);
-}
-///////////////////////////////////////////////
-
+//! Utility function, presents and runs unit tests to tty
 bool RunTests(TestFunc func, const char * testTitle, int& outSucceses, int& outTotals)
 {
     printf("***********************\n");
@@ -181,7 +28,6 @@ bool RunTests(TestFunc func, const char * testTitle, int& outSucceses, int& outT
     printf("Result: %s", result ? "SUCCESS" : "FAIL");
     printf("\n-------------------------\n\n");
     return result;
-
 }
 
 int main()
@@ -190,9 +36,9 @@ int main()
     int total = 0;
 #define RUN_TEST(fn, name) RunTests(fn, name, successes, total)
     
-    ///////////////////////////////////////////////////////////
-    // UNIT TESTS - add here your utils unit tests executions//
-    ///////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+    // UNIT TESTS - add here your UTILS package unit tests executions//
+    ///////////////////////////////////////////////////////////////////
     
     //memcpy
     RUN_TEST(UNIT_TEST_MemcpyUnitTest1,"MemCpy1");
