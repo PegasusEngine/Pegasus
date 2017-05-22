4369941870336b14055b0ccb271ab4ea6ca3a914
Kleber Garcia <kecho_garcia@hotmail.com>
2017-05-21 21:03:29 -0400

    Adding flags for face culling of the box.

--- Source/Pegasus/Mesh/Generator/BoxGenerator.cpp
+++ Source/Pegasus/Mesh/Generator/BoxGenerator.cpp
@@ -19,6 +19,12 @@ namespace Mesh {
 BEGIN_IMPLEMENT_PROPERTIES(BoxGenerator)
     IMPLEMENT_PROPERTY(BoxGenerator, CubeExtends)
     IMPLEMENT_PROPERTY(BoxGenerator, FaceSubdivision)
+    IMPLEMENT_PROPERTY(BoxGenerator, EnableFace0)
+    IMPLEMENT_PROPERTY(BoxGenerator, EnableFace1)
+    IMPLEMENT_PROPERTY(BoxGenerator, EnableFace2)
+    IMPLEMENT_PROPERTY(BoxGenerator, EnableFace3)
+    IMPLEMENT_PROPERTY(BoxGenerator, EnableFace4)
+    IMPLEMENT_PROPERTY(BoxGenerator, EnableFace5)
 END_IMPLEMENT_PROPERTIES(BoxGenerator)
 
 //----------------------------------------------------------------------------------------
@@ -31,6 +37,12 @@ BoxGenerator::BoxGenerator(Pegasus::Alloc::IAllocator* nodeAllocator,
     BEGIN_INIT_PROPERTIES(BoxGenerator)
         INIT_PROPERTY(CubeExtends)
         INIT_PROPERTY(FaceSubdivision)
+        INIT_PROPERTY(EnableFace0)
+        INIT_PROPERTY(EnableFace1)
+        INIT_PROPERTY(EnableFace2)
+        INIT_PROPERTY(EnableFace3)
+        INIT_PROPERTY(EnableFace4)
+        INIT_PROPERTY(EnableFace5)
     END_INIT_PROPERTIES()
 }
 
@@ -47,6 +59,15 @@ void BoxGenerator::GenerateData()
     using namespace Pegasus::Math;
     
     PEGASUS_EVENT_DISPATCH(this, MeshOperationEvent, MeshOperationEvent::BEGIN);
+
+    unsigned int faceEnableMask =
+                     (GetEnableFace0() ? 1 : 0) 
+                   |((GetEnableFace1() ? 1 : 0) << 1)
+                   |((GetEnableFace2() ? 1 : 0) << 2)
+                   |((GetEnableFace3() ? 1 : 0) << 3)
+                   |((GetEnableFace4() ? 1 : 0) << 4)
+                   |((GetEnableFace5() ? 1 : 0) << 5)
+    ;
     
     MeshDataRef meshData = GetData();
     PG_ASSERT(meshData != nullptr); 
@@ -69,29 +90,6 @@ void BoxGenerator::GenerateData()
 
     Vertex * stream = meshData->GetStream<Vertex>(0);
 
-    //generate front face (XY,Z=1)
-    int vertCountInt = subdivisionCount + 1;
-    float vertCountFloat = static_cast<float>(vertCountInt);
-    float len = static_cast<float>(subdivisionCount);
-    float halfLen = 0.5f*len;
-    float invHalfLen = 1.0f / halfLen;
-    {
-        int i = 0;
-        for (float r = 0.0f; r < vertCountFloat; r += 1.0f, ++i)
-        {
-            int j = 0;
-            for (float c = 0.0f; c < vertCountFloat; c += 1.0f, ++j)
-            {
-                float x = invHalfLen*(c - halfLen);
-                float y = -invHalfLen*(r - halfLen);
-                int idx = i * vertCountInt + j;
-                stream[idx].position = Vec4(x,y,1.0f,1.0f);
-                stream[idx].normal = Vec3(0.0f,0.0f,1.0f);
-                stream[idx].uv = Vec2(x,y)*0.5f + 0.5f;
-            }
-        }
-    }
-
     static const int sFaceSwizzles[][3] =
     { /*x  y  z */
         { 0, 1, 2 },
@@ -101,42 +99,63 @@ void BoxGenerator::GenerateData()
         { 1, 2, 0 },
         { 1, 2, 0 }
     };
-    //copy and rotate front face on other faces
-    for (int face = 5; face >= 0; --face)
+
+    //generate front face (XY,Z=1)
+    int vertCountInt = subdivisionCount + 1;
+    float vertCountFloat = static_cast<float>(vertCountInt);
+    float len = static_cast<float>(subdivisionCount);
+    float halfLen = 0.5f*len;
+    float invHalfLen = 1.0f / halfLen;
+    int currIdx = 0;
+    int faceOffsets[6];
+    for (int face = 0; face < 6; ++face)
     {
-        int offset = face * mVertsPerFace;
-        float sign = (face % 2) == 0 ? 1.0f : -1.0f;
-        for (int v = 0; v < mVertsPerFace; ++v)
+        if (!((faceEnableMask >> face) & 1))
         {
-            Vertex* targetVert = stream + offset + v;
-            const Vec4& posCpy = stream[v].position;
-            const Vec3& norCpy = stream[v].normal;
-            const Vec2& uvCpy = stream[v].uv;
-            targetVert->position = Vec4(sign*Vec3(posCpy.v[sFaceSwizzles[face][0]], posCpy.v[sFaceSwizzles[face][1]], posCpy.v[sFaceSwizzles[face][2]]) * GetCubeExtends(),1.0f);
-            targetVert->normal = sign*Vec3(norCpy.v[sFaceSwizzles[face][0]], norCpy.v[sFaceSwizzles[face][1]], norCpy.v[sFaceSwizzles[face][2]]);
-            targetVert->uv = uvCpy;
+            continue;
+        }
+        faceOffsets[face] = currIdx;
+        int i = 0;
+        float sign = (face % 2) == 0 ? 1.0f : -1.0f;
+        for (float r = 0.0f; r < vertCountFloat; r += 1.0f, ++i)
+        {
+            int j = 0;
+            for (float c = 0.0f; c < vertCountFloat; c += 1.0f, ++j)
+            {
+                float x = invHalfLen*(c - halfLen);
+                float y = -invHalfLen*(r - halfLen);
+                Vec4 pos(x,y,1.0f,1.0f);
+                Vec3 norm(0.0f,0.0f,1.0f);
+                stream[currIdx].position = Vec4(sign*Vec3(pos.v[sFaceSwizzles[face][0]], pos.v[sFaceSwizzles[face][1]], pos.v[sFaceSwizzles[face][2]]) * GetCubeExtends(),1.0f);
+                stream[currIdx].normal = sign*Vec3(norm.v[sFaceSwizzles[face][0]], norm.v[sFaceSwizzles[face][1]], norm.v[sFaceSwizzles[face][2]]);
+                stream[currIdx].uv = Vec2(x,y)*0.5f + 0.5f;
+                currIdx++;
+            }
         }
     }
-
 
 	//set the index data
     const short indexesPerFace = 6;
     meshData->AllocateIndexes(indexesPerFace * subdivisionCount * subdivisionCount * 6/*faces*/);
     unsigned short * idx = meshData->GetIndexBuffer();
     int indexOffset = 0;
-    for (unsigned short f = 0; f < 6; ++f)
+    for (unsigned short face = 0; face < 6; ++face)
     {
+        if (!((faceEnableMask >> face) & 1))
+        {
+            continue;
+        }
         for (int j = 0; j < subdivisionCount; ++j)
         {
             for (int i = 0; i < subdivisionCount; ++i)
             {
-                int offset = f*mVertsPerFace + i * vertCountInt + j;                
+                int offset = faceOffsets[face] + i * vertCountInt + j;
                 const unsigned short offsetS = (unsigned short)offset;
                 const unsigned short a = offsetS;
                 const unsigned short b = a + 1;
                 const unsigned short c = a + (unsigned short)vertCountInt;
                 const unsigned short d = c + 1;
-                if ((f % 2) == 0)
+                if ((face % 2) == 0)
                 {
                     idx[indexOffset++] = a;
                     idx[indexOffset++] = c;
