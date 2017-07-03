dbff49750c58defcd41e76f8bb6c65b02f8a1d2c
Kleber Garcia <kecho_garcia@hotmail.com>
2017-07-03 01:39:47 -0400

    Work on deferred renderer: simple light evaluation.

--- Data/TestApp1/Imported/RenderSystems/Lighting/DeferredApplyLights.cs
+++ Data/TestApp1/Imported/RenderSystems/Lighting/DeferredApplyLights.cs
@@ -5,14 +5,14 @@
 #define THREADS_X 32
 #define THREADS_Y 32
 
+#include "RenderSystems/Camera/Common.h"
 #include "RenderSystems/Lighting/LightingCore.h"
 
 cbuffer Constants
 {
     uint2 gTargetDimensions;
-	float2 padding;
+	uint2 gActiveLightCounts;
 };
-
 
 Texture2D<float4> GBuffer0Texture;
 Texture2D<float4> GBuffer1Texture;
@@ -24,20 +24,48 @@ RWTexture2D<float4> OutputBuffer;
 void main(uint3 dti : SV_DispatchThreadId)
 {
     uint2 coords = dti.xy;
+
     if (all(coords < gTargetDimensions))
     {
+		float2 clipSpaceCoords = ((float2)dti.xy) / ((float2)gTargetDimensions) * 0.5 + 0.5;
         GBuffer gbuffers;
         gbuffers.gbuffer0 = GBuffer0Texture[coords];
         gbuffers.gbuffer1 = GBuffer1Texture[coords];
 
         MaterialInfo matInfo = ReadFromGbuffers(gbuffers);
 
+		float clipSpaceDepth = DepthTexture[coords];
+		float4 homogeneousWorldPos = mul(float4(clipSpaceCoords,clipSpaceDepth,1.0),gInvViewProj);
+		float3 worldPos = homogeneousWorldPos.xyz/homogeneousWorldPos.w;
         //TODO: perform light tile sampling and perform computation:
         {
-            float3 finalLighting = matInfo.color * dot(normalize(-float3(0.7,0.7,0.7)), matInfo.worldNormal) + matInfo.smoothness;
-			LightInfo inf = LightInputBuffer[1];
-			
-            OutputBuffer[coords] = float4(finalLighting + inf.attr0.x*0.001, 1.0);
+			float3 diffuse = float3(0.0,0.0,0.0);
+			float3 specular = float3(0.0,0.0,0.0);
+			const uint MaxLights = 16;
+			[loop]
+			for (uint i = 0; i < gActiveLightCounts.x && i < MaxLights; ++i)
+			{
+				LightInfo lightInfo = LightInputBuffer[i];
+				switch(GetLightInfoType(lightInfo))
+				{
+				case LIGHTTYPE_SPHERE:
+					{
+						SphereLight l;
+						GetSphereLight(lightInfo,l);
+						ApplySphereLight(worldPos,matInfo,l,diffuse,specular);
+					}
+					break;
+				case LIGHTTYPE_SPOT:
+				default:
+					{
+						SpotLight l;
+						GetSpotLight(lightInfo,l);
+						ApplySpotLight(worldPos,matInfo,l,diffuse,specular);
+					}
+					break;
+				}
+			} 
+            OutputBuffer[coords] = float4(matInfo.color*diffuse + specular,1.0);
         }
     }
 }
--- Data/TestApp1/Imported/RenderSystems/Lighting/LightingCore.h
+++ Data/TestApp1/Imported/RenderSystems/Lighting/LightingCore.h
@@ -8,6 +8,7 @@
 
 #include "RenderSystems/Lighting/GBuffer.h"
 
+//basic light information
 struct LightInfo 
 {
 	float4 attr0;
@@ -15,6 +16,82 @@ struct LightInfo
 	float4 attr2;
 	int2   attr3;
 };
+
+uint GetLightInfoType(in LightInfo info)
+{
+	return info.attr3.x;
+}
+
+//light types
+struct SphereLight
+{
+	float4 colorAndIntensity;
+	float4 posAndRadius;
+};
+
+struct SpotLight
+{
+	float4 colorAndIntensity;
+	float4 posAndRadius;
+	float4 dirAndAngle;
+};
+
+//casting function
+void GetSphereLight(in LightInfo info, out SphereLight light)
+{
+	light.colorAndIntensity = info.attr0;
+	light.posAndRadius = info.attr1;
+}
+
+void GetSpotLight(in LightInfo info, out SpotLight light)
+{
+	light.colorAndIntensity = info.attr0;
+	light.posAndRadius = info.attr1;
+	light.dirAndAngle = info.attr2; 
+}
+
+//application of lights
+
+float LightDistanceAttenuation(float distanceToLight, float lightRadius)
+{
+	float normalizedDistance = distanceToLight/min(lightRadius,0.00001);
+	return saturate(1.0 / (normalizedDistance*normalizedDistance+0.00001));
+}
+
+void ApplySphereLight(in float3 worldPos, in MaterialInfo material, in SphereLight light, in out float3 diffuse, in out float3 specular)
+{
+	float3 L = light.posAndRadius.xyz-worldPos;
+	float distanceToLight = length(L);
+	L /= distanceToLight;
+	float3 intensity = light.colorAndIntensity.rgb*LightDistanceAttenuation(distanceToLight, light.posAndRadius.w);
+	intensity *= light.colorAndIntensity.xyz*light.colorAndIntensity.w;
+
+	float irradiance = saturate(dot(material.worldNormal,L));
+ 
+	diffuse += irradiance * intensity;
+	specular += 0.0;
+}
+
+void ApplySpotLight(in float3 worldPos, in MaterialInfo material, in SpotLight light, in out float3 diffuse, in out float3 specular)
+{
+	float3 L = light.posAndRadius.xyz-worldPos;
+	float distanceToLight = length(L);
+	L /= distanceToLight;
+	float3 intensity = light.colorAndIntensity.rgb*LightDistanceAttenuation(distanceToLight, light.posAndRadius.w);
+	float irradiance = saturate(dot(material.worldNormal,L));
+
+	float angleDot = dot(L,light.dirAndAngle.xyz);
+
+	//TODO: precompute the max angle in the cpu
+	float h = sqrt(light.posAndRadius.w*light.posAndRadius.w+light.dirAndAngle.w*light.dirAndAngle.w);
+	float maxAngle = light.posAndRadius.w/h;
+
+	irradiance *= (maxAngle-angleDot)/maxAngle;
+
+	diffuse += irradiance * intensity;
+	specular += 0.0;
+
+}
 
 // Light type definitions that must match C++'s LightRig.h LightType enum
 #define LIGHTTYPE_SPHERE 0
@@ -28,6 +105,7 @@ struct LightingOutput
     float4 out : SV_Target0;
 };
 #endif
+
 
 
 LightingOutput ApplyLighting(in MaterialInfo matInfo)
