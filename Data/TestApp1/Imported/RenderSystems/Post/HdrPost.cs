21cf45e110c3629a8c2043e3e81c0dbc2963b763
Kleber Garcia <kecho_garcia@hotmail.com>
2017-07-10 23:51:47 -0400

    Adding filmic tone mapping curve. Increasing poly count of sphere light limit (by karolyn's request) Increasing distance of 2d grid.

--- Data/TestApp1/Imported/RenderSystems/Post/HdrPost.cs
+++ Data/TestApp1/Imported/RenderSystems/Post/HdrPost.cs
@@ -12,10 +12,20 @@ cbuffer Constants
 	float2 padding;
 };
 
+float3 ACESFilm( float3 x )
+{
+    float a = 2.51f;
+    float b = 0.03f;
+    float c = 2.43f;
+    float d = 0.59f;
+    float e = 0.14f;
+    return saturate((x*(a*x+b))/(x*(c*x+d)+e));
+}
+
 float4 ToneMap(float4 hdrColor)
 {
 	//todo: implement tone mapping, for now just linearizing
-	return hdrColor/1.0;
+	return float4(ACESFilm(0.1*hdrColor.rgb),hdrColor.a);
 }
 
 #define TILE_SIZE 32
