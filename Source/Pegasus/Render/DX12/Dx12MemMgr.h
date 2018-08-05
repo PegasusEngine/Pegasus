b5842ea6753bfa5364a89832370bd925b9ebca0b
KECHO-PUTER\kecho <kecho_garcia@hotmail.com>
2018-08-05 19:41:26 -0400

    Adding boiler plate for management of simple Resource tables.

--- Source/Pegasus/Render/DX12/Dx12MemMgr.h
+++ Source/Pegasus/Render/DX12/Dx12MemMgr.h
@@ -25,6 +25,7 @@ class Dx12Device;
 class Dx12MemMgr
 {
 public:
+
     struct Handle
     {
         D3D12_DESCRIPTOR_HEAP_TYPE type;
@@ -32,15 +33,36 @@ public:
         UINT index;
     };
 
+    enum TableType
+    {
+        TableTypeSrv, TableTypeUav, TableTypeCbv
+    };
+
+    struct Table
+    {
+        D3D12_CPU_DESCRIPTOR_HANDLE baseHandle;
+        D3D12_GPU_DESCRIPTOR_HANDLE baseHandleGpu;
+        TableType tableType = TableTypeSrv;
+        UINT baseIdx = 0;   
+        UINT count = 0;
+    };
+
     Dx12MemMgr(Dx12Device* device);
     ~Dx12MemMgr();
     
-    Handle AllocateRenderTarget();
+    inline Handle AllocateRenderTarget() { return AllocInternal(D3D12_DESCRIPTOR_HEAP_TYPE_RTV); }
+    inline Handle AllocateSrvOrUavOrCbv() { return AllocInternal(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV); }
+
+    Table AllocateTable(TableType tableType, const Handle* cpuHandles, UINT cpuHandleCounts);
+    Table AllocateTableCopy(const Table& srcTable);
+
+    void Delete(const Table& t);
     void Delete(Handle h);
 
 private:
 
     Handle AllocInternal(D3D12_DESCRIPTOR_HEAP_TYPE type);
+    Table AllocEmptyTable(UINT count, TableType tableType);
 
     Dx12Device* mDevice;
 
@@ -53,10 +75,27 @@ private:
         std::vector<UINT> freeSpots;
     };
 
+    struct GpuHeapContainer
+    {
+        UINT incrSize = 0;
+        UINT lastIndex = 0;
+        D3D12_DESCRIPTOR_HEAP_DESC desc;
+        CComPtr<ID3D12DescriptorHeap> heap;
+        std::vector<Table> freeSpots;
+    };
+
+    enum GpuHeapTypes
+    {
+        GpuHeapCbv,
+        GpuHeapSrv,
+        GpuHeapUav,
+        MaxGpuHeapTypes
+    };
+
     typedef std::vector<HeapContainer> HeapList;
 
     HeapContainer mHeaps[D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES];
-
+	GpuHeapContainer mGpuHeap;
 };
 
 }
