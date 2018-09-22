cfb23941681d8949029af8b5e62990dff6dec72b
KECHO-PUTER\kecho <kecho_garcia@hotmail.com>
2018-09-22 07:28:53 -0400

    More boiler plate in dx12 pso

--- Source/Pegasus/Render/DX12/Dx12Pso.cpp
+++ Source/Pegasus/Render/DX12/Dx12Pso.cpp
@@ -7,17 +7,37 @@ namespace Render
 {
 
 Dx12Pso::Dx12Pso(Dx12Device* device)
-: Core::RefCounted(device->GetAllocator()), mDevice(device)
+: Core::RefCounted(device->GetAllocator()), mDevice(device), mPso(nullptr)
 {
 }
 
 Dx12Pso::~Dx12Pso()
 {
+    if (mPso != nullptr)
+    {
+        mPso->Release();
+    }
 }
 
-void Dx12Pso::Compile(const Dx12PsoDesc& desc)
+bool Dx12Pso::Compile(const Dx12PsoDesc& desc)
 {
     mDesc = desc;
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC psoGraphicsDesc;
+
+    if (mPso != nullptr)
+    {
+        mPso->Release();
+        mPso = nullptr;
+    }
+
+    if (desc.program == nullptr || !desc.program->IsValid())
+        return nullptr;
+    
+    HRESULT result = mDevice->GetD3D()->CreateGraphicsPipelineState(&psoDesc, __uiidof(ID3D12PipelineState), reinterpret_cast<void**>(&mPso));
+    if (result != S_OK)
+        return false;
+    else
+        return true; 
 }
 
 }
