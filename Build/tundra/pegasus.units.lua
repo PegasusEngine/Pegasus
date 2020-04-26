dac0c7db7463d8225f2a199ca83b86b7a5776c9c
Kleber Garcia <kecho_garcia@hotmail.com>
2020-04-26 15:37:07 -0400

    Adding all module dependencies and all lib modules now compile in debug/production and release

--- Build/tundra/pegasus.units.lua
+++ Build/tundra/pegasus.units.lua
@@ -1,28 +1,98 @@
 require 'tundra.syntax.glob'
 
-local function PegasusLib(name)
+local function PegasusLib(name, srcFolder, srcFolderIsRecursive, deps)
     return StaticLibrary {
         Name = name,
         Includes = { "Include" },
         Sources = {
             Glob {
-                Dir = "Source/Pegasus/" .. name,
-                Extensions = { ".cpp", ".h" }
+                Dir = "Source/Pegasus/" .. srcFolder,
+                Extensions = { ".cpp", ".h" },
+                Recursive = srcFolderIsRecursive 
+            },
+            Glob {
+                Dir = "Include/Pegasus/" .. srcFolder,
+                Extensions = { ".h" },
+                Recursive = srcFolderIsRecursive 
             }
         },
+        Depends = deps,
         Env = {
             CXXOPTS = {
-              { "/FIPegasus/Preprocessor.h"; Config = "win32-msvc-*" },
-              { "/FIPegasus/PegasusInternal.h"; Config = "win32-msvc-*" },
+              {
+                  Config = "win32-msvc-*",
+                  {
+                      "/FIPegasus/Preprocessor.h",
+                      "/FIPegasus/PegasusInternal.h",
+                      "/EHsc",
+                      "/wd5033", --ignore register keyword deprecation warning
+                      "/wd4514", -- ignore unreferenced symbols warning
+                  }
+              },
             },
         }
     }
 end
 
-PegasusLib("Allocator")
-PegasusLib("Core")
-PegasusLib("Math")
+local pegasus_modules  = {
+    "Allocator",
+    "Application",
+    "AssetLib", 
+    "BlockScript",
+    "Core",
+    "Graph",
+    "Math",
+    "Memory",
+    "Mesh",
+    "Pegasus",
+    "PropertyGrid",
+    "Render",
+    "RenderSystems",
+    "Shader",
+    "Sound",
+    "Texture",
+    "Timeline",
+    "Utils",
+    "Window",
+}
 
-Default "Allocator"
-Default "Core"
-Default "Math"
+local pegasus_modules_dependencies  = {
+    Application =  { "Allocator", "AssetLib", "BlockScript", "Core", "Graph", "Math", "Memory", "Mesh", "PropertyGrid", "RenderSystems", "Shader", "Sound", "Texture", "Timeline", "Utils", "Window" },
+    AssetLib  = { "Allocator", "Core", "Memory", "Utils" },
+    BlockScript = { "Allocator", "Core", "Memory", "Utils" },
+    Core = { "Allocator" },
+    Graph = { "Allocator", "Core", "Math", "Memory", "PropertyGrid", "Utils" },
+    Math = { "Core" },
+    Memory = { "Allocator", "Core" },
+    Mesh = { "Allocator", "AssetLib", "Core", "Graph", "Math", "Memory", "PropertyGrid", "Utils" },
+    Pegasus = { "Allocator", "Application", "AssetLib", "BlockScript", "Core", "Graph", "Math", "Memory", "Mesh", "PropertyGrid", "Render", "RenderSystems", "Shader", "Sound", "Texture", "Timeline", "Utils", "Window" },
+    PropertyGrid = { "Allocator", "AssetLib", "Core", "Math", "Memory", "Utils" },
+    Render = { "Allocator", "Core", "Graph", "Math", "Memory", "Mesh", "PropertyGrid", "Shader", "Texture", "Utils" },
+    RenderSystems = { "Allocator", "AssetLib", "BlockScript", "Core", "Graph", "Math", "Memory", "Mesh", "PropertyGrid", "Shader", "Texture", "Utils" },
+    Shader = { "Allocator", "Core", "Graph", "Math", "PropertyGrid" },
+    Sound = { "Allocator", "Core" },
+    Texture = { "Allocator", "AssetLib", "Core", "Graph", "Math", "Memory", "PropertyGrid", "Utils" },
+    Timeline = { "Allocator", "AssetLib", "BlockScript", "Core", "Graph", "Math", "PropertyGrid", "Sound", "Window" },
+    Utils = { "Allocator", "Core", "Math" },
+    Window = { "Allocator", "Core", "Memory", "Render" }
+}
+
+--folder and recursive flag
+local pegasus_modules_src_folders = {
+    Pegasus = { "", false }
+}
+
+for i, moduleName in ipairs(pegasus_modules) do
+    local deps = pegasus_modules_dependencies[moduleName]
+    local srcFolderRecursePair = pegasus_modules_src_folders[moduleName]
+    local srcFolder = (srcFolderRecursePair == nil) and moduleName or srcFolderRecursePair[1]
+    local isRecursive = (srcFolderRecursePair == nil) and true or srcFolderRecursePair[2]
+    PegasusLib(
+        moduleName,
+        srcFolder,
+        isRecursive,
+        (deps == nil) and {} or deps --only pass dependencies if they exist
+     )
+    Default(moduleName)
+end
+
--- tundra.lua
+++ tundra.lua
@@ -8,7 +8,7 @@ Build
         {
             Name="win32-msvc",
             --Tools = { "msvc-vs2019" }
-            Tools = { "msvc-vs2017" },
+            Tools = { { "msvc-vs2017"; TargetArch="x86" } },
             SupportedHosts = { "windows" },
             DefaultOnHost = "windows",
             Includes = "CPPPATH",
@@ -16,6 +16,12 @@ Build
     },
     
     Env = {
-        SRC_DIR = "Source"
+        SRC_DIR = "Source",
+        CXXOPTS = {
+            {
+                { "/std:c++17", "/WX", "/W3" },
+                Config = "win32-msvc-*"
+            }
+        },
     }
 }
